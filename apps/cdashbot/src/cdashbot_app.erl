14d194f0f7841c5a0b7fbd079d0104704abc594c
Gorskin Ilya <revent82@gmail.com>
2014-10-07 16:08:29 +0600

    small fix tabs.

--- apps/cdashbot/src/cdashbot_app.erl
+++ apps/cdashbot/src/cdashbot_app.erl
@@ -12,13 +12,18 @@
 start() -> 
 	application:start(inets),
 	application:start(exmpp),
-	application:start(lager), 
+	application:start(lager),
+  application:start(jsonx),
 	application:start(cdashbot),
-	application:start(api_module), 
 	ok.
 
 start(_StartType, _StartArgs) ->
-    cdashbot_sup:start_link().
+  cdashbot_sup:start_link(cdashbot_wrk).
 
 stop(_State) ->
-    ok.
+	application:stop(cdasbot),
+	application:stop(lager),
+	application:stop(exmpp),
+  application:stop(jsonx),
+  application:stop(inets),
+  ok.
